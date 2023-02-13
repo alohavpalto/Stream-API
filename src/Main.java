44cff566010edc314b6035520ba957aaee6c0ddd
OtlapPalto <marby1palto@gmail.com>
2023-02-13 23:30:19 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+    List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson",
+        "Brown");
+    Collection<Person> persons = new ArrayList<>();
+    for (
+        int i = 0;
+        i < 10_000_000; i++) {
+      persons.add(new Person(
+          names.get(new Random().nextInt(names.size())),
+          families.get(new Random().nextInt(families.size())),
+          new Random().nextInt(100),
+          Sex.values()[new Random().nextInt(Sex.values().length)],
+          Education.values()[new Random().nextInt(Education.values().length)])
+      );
+    }
+
+    long countMinors = persons.stream()
+        .filter(person -> person.getAge() < 18)
+        .count();
+    //System.out.println(countMinors);
+
+    List<String> conscript = persons.stream()
+        .filter(person -> person.getSex() == Sex.MAN)
+        .filter(person -> person.getAge() >= 18 && person.getAge() < 27)
+        .map(Person::getFamily)
+        .collect(Collectors.toList());
+    //System.out.println(conscript);
+
+    List<Person> workable = persons.stream()
+        .filter(person -> person.getAge() >= 18)
+        .filter(person -> person.getEducation() == Education.HIGHER)
+        .filter(person -> person.getSex() == Sex.MAN && person.getAge() < 65 ||
+            person.getSex() == Sex.WOMAN && person.getAge() < 60)
+        .sorted(Comparator.comparing(Person::getFamily))
+        .collect(Collectors.toList());
+    System.out.println(workable);
+  }
+}
